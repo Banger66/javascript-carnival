7dbf61b9d9942416f3ab75b2b11cb202d24fadba
Banger66 <airhorns95@gmail.com>
2022-05-10 10:29:33 +1200

    Unicorns balloons inflate and they thank you

--- inflate-the-unicorn/inflate-the-unicorn.js
+++ inflate-the-unicorn/inflate-the-unicorn.js
@@ -1,5 +1,29 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Inflate The Unicorn!")
+console.log('Inflate The Unicorn!')
+
+let inflate = document.getElementsByClassName('inflate-an-image')
+
+// Make clicking on a unicorn call the inflateClicked() function
+
+for (let i = 0; i < inflate.length; i++) {
+  inflate[i].onclick = inflateClicked
+}
+
+// If image source = unicorn-0, change source to unicorn-1 etc up to unicorn 3
+
+function inflateClicked(e) {
+  let uni = e.target
+  console.log(inflate)
+  if (uni.src.match('unicorn-0.png')) {
+    uni.src = './images/unicorn-1.png'
+  } else if (uni.src.match('unicorn-1.png')) {
+    uni.src = 'images/unicorn-2.png'
+  } else if (uni.src.match('unicorn-2.png')) {
+    uni.src = 'images/unicorn-3.png'
+  } else if (uni.src.match('unicorn-3.png')) {
+    alert('The Unicorn says Thank You!')
+  }
+}
