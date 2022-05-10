a3088861771e73eba5acf97021b2ad2abddf394b
Banger66 <airhorns95@gmail.com>
2022-05-10 16:02:22 +1200

    COmplete game, add timer and counter

--- whack-a-mole/whack-a-mole.js
+++ whack-a-mole/whack-a-mole.js
@@ -13,6 +13,17 @@ mole.src = 'mole.PNG'
 mole.style.width = '100%'
 mole.style.height = '100%'
 
+//Add stupid sound effect
+let sound = new Audio('whack-audio.wav')
+
+//Give value for clicks(used as a Moles Whacked counter)
+let clicks = 0
+
+//Start timer at 10 seconds, display time next to time left div, reduce countdown function by 1 every 1000ms
+let timeLeft = 10
+let timer = document.getElementById('timer')
+let timerID = setInterval(countdown, 1000)
+
 function showMole() {
   //Create variable to store cells
   let cells = document.getElementsByTagName('td')
@@ -23,3 +34,26 @@ function showMole() {
   randomCell.appendChild(mole)
 }
 showMole()
+
+// When mole gets clicked on, remove mole, play sound effect, then run showMole to respawn mole
+mole.onclick = function whackedMole() {
+  mole.remove()
+  sound.play()
+  showMole()
+  //Add 1 to clicks and display value next to Moles Whacked
+  clicks += 1
+  document.getElementById('counter').innerHTML = clicks
+}
+
+//Countdown timer function
+function countdown() {
+  //If timer gets to 0, clear timer and give alert
+  if (timeLeft == -1) {
+    clearTimeout(timerID)
+    alert('Times Up! You whacked ' + clicks + ' Moles!')
+    //display timeleft in timer and reduce by 1
+  } else {
+    timer.innerHTML = timeLeft
+    timeLeft--
+  }
+}
