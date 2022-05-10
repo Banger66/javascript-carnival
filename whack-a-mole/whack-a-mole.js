c8e08db9fb1f82c70681d2339f6fc3e78138911a
Banger66 <airhorns95@gmail.com>
2022-05-10 13:53:58 +1200

    Show mole in random cell

--- whack-a-mole/whack-a-mole.js
+++ whack-a-mole/whack-a-mole.js
@@ -1,5 +1,25 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Whack-a-Mole!")
+// Game Code
+
+console.log('Whack-a-Mole!')
+
+// Create mole
+let mole = document.createElement('img')
+mole.src = 'mole.PNG'
+//Make mole.PNG same size as cell
+mole.style.width = '100%'
+mole.style.height = '100%'
+
+function showMole() {
+  //Create variable to store cells
+  let cells = document.getElementsByTagName('td')
+  //Create random number between 0-24 (25 numbers in total)
+  let randomIndex = Math.floor(Math.random() * 24)
+  //Show the mole in a random cell
+  let randomCell = cells[randomIndex]
+  randomCell.appendChild(mole)
+}
+showMole()
