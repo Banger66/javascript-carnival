c4ba7f951f1299c1e1783a0de8de3e53e94ca721
Banger66 <airhorns95@gmail.com>
2022-05-11 14:03:20 +1200

    Change head of clown using arrow keys

--- dress-the-clown/dress-the-clown.js
+++ dress-the-clown/dress-the-clown.js
@@ -1,5 +1,40 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Dress The Clown!")
+console.log('Dress The Clown!')
+
+let headIndex = 0
+
+//Change head
+function changeClownHead() {
+  let head = document.getElementById('head')
+  let headSrc = './images/head' + headIndex + '.png'
+  head.src = headSrc
+}
+
+changeClownHead()
+
+document.onkeydown = checkKey
+
+function checkKey(e) {
+  e = e || window.event
+
+  if (e.code == 'ArrowUp') {
+    // up arrow
+  } else if (e.code == 'ArrowDown') {
+    // down arrow
+  } else if (e.code == 'ArrowLeft') {
+    if (headIndex == 0) {
+      headIndex = 5
+    }
+    headIndex--
+    changeClownHead()
+  } else if (e.code == 'ArrowRight') {
+    headIndex++
+    changeClownHead()
+    if (headIndex == 5) {
+      headIndex = 0
+    }
+  }
+}
